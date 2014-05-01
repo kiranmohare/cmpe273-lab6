bc6458fcee0b1145074ec4d489869daac2eb67b6
kiranmohare <kiranmohare@users.noreply.github.com>
2014-04-30 23:46:55 -0700

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,29 +1,70 @@
 package edu.sjsu.cmpe.cache.client;
-import com.google.common.hash.*;
 
 import java.util.ArrayList;
+import java.util.SortedMap;
+import java.util.TreeMap;
+
+import com.google.common.hash.HashCode;
+import com.google.common.hash.HashFunction;
+import com.google.common.hash.Hashing;
 
 public class Client {
 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Starting Cache Client...");
-	ArrayList<CacheServiceInterface> servers = new ArrayList<>();
-	servers.add(new DistributedCacheService("http://localhost:3000"));
-	servers.add(new DistributedCacheService("http://localhost:3001"));
-	servers.add(new DistributedCacheService("http://localhost:3002"));
+private final static SortedMap<Integer, String> ring =
+new TreeMap<Integer, String>();
+private static HashFunction hf = Hashing.md5();
+private static ArrayList<String> servers = new ArrayList<String>();
+char[] ch = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
 
-	ConsistentHash hash = new ConsistentHash(Hashing.md5(), 3, servers);
-	for(long i = 1; i <= 10; i++) {
-	    String aux = Character.toString((char)('a' + i - 1));
-	    hash.get(i).put(i, aux);
-	    int bucket = servers.indexOf(hash.get(i));
-	    System.out.println(i + " routed to: " + ((DistributedCacheService) servers.get(bucket)).getUrl());
-	}
- 	for(long i = 1; i <= 10; i++) {
-	    System.out.println("GET(" + i + ") ==> " + hash.get(i).get(i));
-	}
+public static void main(String[] args) throws Exception {
 
-        System.out.println("Existing Cache Client...");
-    }
+servers.add("http://localhost:3000");
+servers.add("http://localhost:3001");
+servers.add("http://localhost:3002");
+for(int i=0;i<servers.size();i++)
+{
+System.out.println("Adding the server : "+servers.get(i));
+add(servers.get(i), i);
+}
 
+for(int j=0 ; j<10 ; j++)
+{
+int bucket = Hashing.consistentHash(Hashing.md5().hashInt(j), ring.size());
+String server= get(bucket);
+System.out.println("routed to Server: " + server);
+CacheServiceInterface cache = new DistributedCacheService(server);
+cache.put(j+1, String.valuesOfch[j]);
+String value = cache.get(j+1);
+System.out.println("get("+(j+1)+") => " + value);
+
+}
+
+System.out.println("Exiting Cache Client...");
+}
+
+
+
+public static void add(String server, int i) {
+HashCode hc = hf.hashLong(i);
+ring.put(hc.asInt(), server);
+}
+
+public static void remove(int key) {
+int hash = hf.hashLong(key).asInt();
+ring.remove(hash);
+}
+
+public static String get(Object key) {
+if (ring.isEmpty()) {
+return null;
+}
+int hash = hf.hashLong((Integer)key).asInt();
+if (!ring.containsKey(hash)) {
+SortedMap<Integer, String> tailMap =
+ring.tailMap(hash);
+hash = tailMap.isEmpty() ?
+ring.firstKey() : tailMap.firstKey();
+}
+return ring.get(hash);
+} 
 }
