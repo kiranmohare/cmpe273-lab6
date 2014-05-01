e8b2dcafd92aa0008eeec2ea834df4e0e6391f53
kiranmohare <kiranmohare@users.noreply.github.com>
2014-04-30 22:25:22 -0700

    Update Client.java

--- client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
+++ client/src/main/java/edu/sjsu/cmpe/cache/client/Client.java
@@ -1,17 +1,27 @@
 package edu.sjsu.cmpe.cache.client;
+import com.google.common.hash.*;
+
+import java.util.ArrayList;
 
 public class Client {
 
     public static void main(String[] args) throws Exception {
         System.out.println("Starting Cache Client...");
-        CacheServiceInterface cache = new DistributedCacheService(
-                "http://localhost:3000");
+	ArrayList<CacheServiceInterface> servers = new ArrayList<>();
+	servers.add(new DistributedCacheService("http://localhost:3000"));
+	servers.add(new DistributedCacheService("http://localhost:3001"));
+	servers.add(new DistributedCacheService("http://localhost:3002"));
 
-        cache.put(1, "foo");
-        System.out.println("put(1 => foo)");
-
-        String value = cache.get(1);
-        System.out.println("get(1) => " + value);
+	ConsistentHash hash = new ConsistentHash(Hashing.md5(), 3, servers);
+	for(long i = 1; i <= 10; i++) {
+	    String aux = Character.toString((char)('a' + i - 1));
+	    hash.get(i).put(i, aux);
+	    int bucket = servers.indexOf(hash.get(i));
+	    System.out.println(i + " routed to: " + ((DistributedCacheService) servers.get(bucket)).getUrl());
+	}
+ 	for(long i = 1; i <= 10; i++) {
+	    System.out.println("GET(" + i + ") ==> " + hash.get(i).get(i));
+	}
 
         System.out.println("Existing Cache Client...");
     }
